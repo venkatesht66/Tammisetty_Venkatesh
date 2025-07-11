ae5e1f766b6bef390ea977a547005c988a874970
venkatesht66 <167290831+venkatesht66@users.noreply.github.com>
2025-07-11 10:00:11 +0530

    Update Problem-3.py

--- Problem-3.py
+++ Problem-3.py
@@ -1,1 +1,14 @@
+n = int(input())
 
+if n % 2 != 0:
+    number_of_ele = n
+else:
+    number_of_ele = n - 1
+
+l = []
+
+for i in range(number_of_ele):
+    odd_number = 2 * i + 1
+    l.append(odd_number)
+
+print(", ".join(str(i) for i in l))
