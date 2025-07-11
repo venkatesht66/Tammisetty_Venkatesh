1d02004fa1ae33c8f3ab9514d98db6fd1234b606
venkatesht66 <167290831+venkatesht66@users.noreply.github.com>
2025-07-11 10:02:38 +0530

    Update Problem-4.py

--- Problem-4.py
+++ Problem-4.py
@@ -1,1 +1,12 @@
+numbers = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
 
+d = {}
+
+for i in range(1, 10):
+    count = 0
+    for j in numbers:
+        if j % i == 0:
+            count += 1
+    d[i] = count
+
+print(d)
